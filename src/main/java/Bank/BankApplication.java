b7f5098d91ebb4804cd7db1a0b3b856bc9098f76
avasanth123456-wq <avasanth123456@gmail.com>
2026-08-24 16:59:19 +0530

    bank

--- src/main/java/Bank/BankApplication.java
+++ src/main/java/Bank/BankApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankApplication {
+public class gitBankApplication {
 
     public static void main(String[] args) {
 
